2a2a836bae84d834040e96c4bc5472a96b0264a0
davidruncode <daguz.91@gmail.com>
2025-10-20 21:37:22 -0400

    DB Connections

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { testConnection } from './db/db';
+import { testConnection } from './db/db.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
