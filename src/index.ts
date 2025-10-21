5654485e2c8ecc2d16c04bca7e566e9e14ae5ca7
davidruncode <daguz.91@gmail.com>
2025-10-21 18:27:22 -0400

    DB Connections

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
-import express from 'express';
+import express, { type Request, type Response } from 'express';
 import { testConnection } from './db/db';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT: number = Number(process.env.PORT) || 3000;
 
 //Middleware base
 app.use(express.json());
 
 //Ruta de prueba temporal
-app.get("/", (req, res) => {
-    res.send("✅ Smart Configurator Backend is running");
+app.get("/", (request: Request, response: Response): void=> {
+    response.send("✅ Smart Configurator Backend is running");
 });
 
 // Iniciar el servidor + probar conexion a DB
-app.listen(PORT, async () => {
+app.listen(PORT, () => {
     console.log(`🚀 Server is running on port ${PORT}`);
-    await testConnection();
+    void testConnection();
 })
