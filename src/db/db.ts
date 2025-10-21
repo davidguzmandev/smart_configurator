ec38c35be519420fa97d67b126135329089020bf
davidruncode <daguz.91@gmail.com>
2025-10-20 20:49:43 -0400

    DB Connections

---
+++ src/db/db.ts
@@ -0,0 +1,21 @@
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const pool = new Pool ({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: Number(process.env.DB_PORT),
+})
+
+export async function testConnection() {
+    try {
+        const result = await pool.query("SELECT NOW()");
+        console.log("✅ Database connection successful:", result.rows[0].now);
+    } catch (error) {
+        console.error("❌ Database connection failed:", error);
+        process.exit(1);
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import { testConnection } from './db/db';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//Middleware base
+app.use(express.json());
+
+//Ruta de prueba temporal
+app.get("/", (req, res) => {
+    res.send("✅ Smart Configurator Backend is running");
+});
+
+// Iniciar el servidor + probar conexion a DB
+app.listen(PORT, async () => {
+    console.log(`🚀 Server is running on port ${PORT}`);
+    await testConnection();
+})
