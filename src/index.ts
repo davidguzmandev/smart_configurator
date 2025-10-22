5481da5edcfc212d99228d4f2c2dccaaeacc6f7d
davidruncode <daguz.91@gmail.com>
2025-10-22 18:35:06 -0400

    Middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,25 @@
-import express, { type Request, type Response } from 'express';
+import express, { type Request, type Response,  type Application } from 'express';
 import { testConnection } from './db/db';
 import cors from 'cors';
+import morgan from 'morgan';
+import tasksRouter from './routes/tasks.routes';
+import { errorHandler } from './middleware/errorHandler';
 
-const app = express();
+const app: Application =  express();
 const PORT: number = Number(process.env.PORT) || 3000;
 
 //Middleware base
 app.use(express.json());
 app.use(cors());
+app.use(morgan('dev'));
 
 //Ruta de prueba temporal
 app.get("/", (request: Request, response: Response): void=> {
     response.send("✅ Smart Configurator Backend is running");
 });
+
+app.use('/api', tasksRouter)
+app.use(errorHandler);
 
 // Iniciar el servidor + probar conexion a DB
 app.listen(PORT, () => {
--- src/routes/tasks.routes.ts
+++ src/routes/tasks.routes.ts
@@ -6,3 +6,5 @@ const router = Router();
 router.post('/generate-part', generatePart);
 router.get('/tasks', listTasks);
 router.patch('/update-task/:id', markTaskAsDone);
+
+export default router;
